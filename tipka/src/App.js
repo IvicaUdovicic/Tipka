ea9b423b13b1355cccf39a8568e3158c03eae651
Ivica Udovicic <udovicic.ivica@gmail.com>
2021-12-17 20:10:04 +0100

    Dohvacanje boja na pritisak, ne radi u chromu

--- tipka/src/App.js
+++ tipka/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [currentColor, setColor] = useState('');
+  const handleClick = async() => {
+  const data = await axios.post('/json/color/random')
+  setColor({
+    data:data,
+    loading:false
+  })
+  console.log(data.data);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick} type="button" className="btn btn-primary">Search</button>
+
+      <ul>
+        <p>a</p>
+      </ul>
+      <input type="text" id="boja"></input>
     </div>
   );
 }
