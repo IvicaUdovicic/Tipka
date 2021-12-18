95ac42908c9ad5488464eeb1b4161f52a5552227
Ivica Udovicic <udovicic.ivica@gmail.com>
2021-12-18 23:48:06 +0100

    tipka minja boje teksta, ispisujem listu ali ne imena boja to moras nac path

--- tipka/src/App.js
+++ tipka/src/App.js
@@ -1,24 +1,62 @@
 /*import logo from './logo.svg';*/
-import './App.css';
-import React, { useState } from 'react';
-import axios from 'axios';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+
+/*function ColorList(){
+ 
+  }
+  
+  
+  return(
+    <div>
+      <button onClick={addColor}>dodaj boju</button>
+      <ul>
+        
+      </ul>
+    </div>
+  )
+}*/
+
 
 function App() {
-  const [currentColor, setColor] = useState('');
-  const handleClick = async() => {
-  const data = await axios.post('/json/color/random')
-  setColor({
-    data:data,
-    loading:false
-  })
-  console.log(data.data);
-}
+  const[color, setColor] = useState([])
+  const addColor = () => {
+    setColor(arr => [...color, `${arr.length}`]);
+    };
+  
+  
+  
+  
+  
+  
+  
+  const [currentColor, setCurrentColor] = useState("");
+  const handleClick = async () => {
+    const data = await axios.get("/json/color/random");
+    setCurrentColor({
+        color: "#" + data.data.new_color,
+        loading:false
+    });
+    console.log(data.data.colors);
+  };
   return (
     <div>
-      <button onClick={handleClick} type="button" className="btn btn-primary">Search</button>
+      <button
+        style={{ color: currentColor ? currentColor.color : 'white'}}
+        onClick={() => {
+          handleClick();
+          addColor();}}
+        className="btn btn-primary"
+        type="button"
+        value="Update"
+      >
+        Search
+      </button>
 
       <ul>
-        <p>a</p>
+      {color.map(color => (
+          <li key={color.data}>{color.value}</li>))}
       </ul>
       <input type="text" id="boja"></input>
     </div>
